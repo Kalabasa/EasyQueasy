36355f8abedb5c5a48d32ed0550e2cddbfb61c4a
Kalabasa <Kalabasa@users.noreply.github.com>
2024-06-22 00:28:08 +1000

    doesnt work

--- app/src/main/java/com/leanrada/easyqueasy/services/AddOverlayView.kt
+++ app/src/main/java/com/leanrada/easyqueasy/services/AddOverlayView.kt
@@ -3,20 +3,23 @@ package com.leanrada.easyqueasy.services
 import android.content.Context
 import android.graphics.PixelFormat
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowManager
+import android.view.WindowManager.LayoutParams
 
 fun addOverlayView(context: Context, view: View, layoutType: Int) {
     val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
-    val layoutParams = WindowManager.LayoutParams()
+    val layoutParams = LayoutParams()
     layoutParams.apply {
-        width = ViewGroup.LayoutParams.MATCH_PARENT
-        height = ViewGroup.LayoutParams.MATCH_PARENT
+        width = LayoutParams.MATCH_PARENT
+        height = LayoutParams.MATCH_PARENT
         format = PixelFormat.TRANSPARENT
         type = layoutType
         flags =
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            LayoutParams.FLAG_NOT_TOUCHABLE or
+                    LayoutParams.FLAG_NOT_FOCUSABLE or
+                    LayoutParams.FLAG_LAYOUT_NO_LIMITS or
+                    LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
     }
 
     windowManager.addView(view, layoutParams)
